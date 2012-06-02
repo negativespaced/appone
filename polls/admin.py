c571578943b0ca31f3edd78cdbad438e4fd71d30
CSS Superman <csssuperman9@gmail.com>
2012-06-02 16:08:55 +0530

    added polls in admin
    let the admin display polls

---
+++ polls/admin.py
@@ -0,0 +1,14 @@
+from polls.models import Poll
+from django.contrib import admin
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Poll, PollAdmin)
+
+from polls.models import Choice
+
+admin.site.register(Choice)
