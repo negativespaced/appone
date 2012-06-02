73da98d28d8356288e611d50d6e363d0d78dc1f5
CSS Superman <csssuperman9@gmail.com>
2012-06-02 15:37:40 +0530

    working on poll application - admin added

--- appone/urls.py
+++ appone/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'appone.views.home', name='home'),
-    # url(r'^appone/', include('appone.foo.urls')),
+    # Example:
+    # (r'^appone/', include('appone.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
